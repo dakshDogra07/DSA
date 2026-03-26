2cdc95df244d5018405cfd6ad68a99152665c577
Daksh Dogra <dakshdogra9b.5@gmail.com>
2026-03-27 02:14:39 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (57.22%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -12,7 +12,7 @@ public:
 
         int repeating =-1, missing = -1;
 
-        for(int i=0; i <=n*n; i++){
+        for(int i=1; i <=n*n; i++){
             if(freq[i] == 2) repeating = i;
             if(freq[i] == 0) missing = i;
         }
